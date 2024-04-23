1a4032898cce81dba877eea9a3dae5792555c909
Pedro Monteiro <josepedromonteiro@outlook.com>
2024-04-24 00:08:12 +0100

    Fix label generation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {json, urlencoded} from "express";
+import { json, urlencoded } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
   app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  await app.listen(3000);
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
