cc439654fc4d06bb180447ef687b195ac33989e7
Pedro Monteiro <josepedromonteiro@outlook.com>
2024-04-24 00:10:57 +0100

    Fix label generation

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
   app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
+  const port = configService.get('PORT') ?? 3000;
+  console.log(`Will listen on port: ${port}`);
   await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
